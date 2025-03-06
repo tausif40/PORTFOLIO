d5aa4a7a10b377ad3f2a33af7aa5953c1010a975
tausif40 <tausif6040@gmail.com>
2025-03-06 23:42:26 +0530

    ui

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -75,7 +75,7 @@ export default function Projects() {
         <h2 className="text-4xl font-bold text-center mb-12 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
           Featured Projects
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 gap-y-14 mb-8">
           {projects.map((project) => (
             <motion.div
               key={project.id}
